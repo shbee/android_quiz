7c903bca90e66dd0dc99982436048f6f327d15f1
Samuel <samuel@exercise.com>
2019-08-20 22:25:53 +0800

    Touch and draw cell grid

--- app/src/main/java/samuel/exercise/homequiz/gameoflife/GameOfLifeActivity.kt
+++ app/src/main/java/samuel/exercise/homequiz/gameoflife/GameOfLifeActivity.kt
@@ -1,15 +1,30 @@
 package samuel.exercise.homequiz.gameoflife
 
 import android.os.Bundle
+import android.view.MotionEvent
 import androidx.appcompat.app.AppCompatActivity
 import samuel.exercise.homequiz.R
+import samuel.exercise.homequiz.view.CellGridView
 
-class GameOfLifeActivity : AppCompatActivity(), GameOfLifeContract.View {
+class GameOfLifeActivity : AppCompatActivity(), GameOfLifeContract.View, CellGridView.CellTouchListener {
 
     override lateinit var presenter: GameOfLifeContract.Presenter
+    private lateinit var gridView: CellGridView
+    private val liveList = mutableListOf<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        gridView = findViewById<CellGridView>(R.id.grid_view).apply {
+            cellTouchListener = this@GameOfLifeActivity
+        }
+    }
+
+    override fun onCellTouched(event: MotionEvent, cellIndex: Int) {
+        if (!liveList.remove(cellIndex)) {
+            liveList.add(cellIndex)
+        }
+        gridView.post { gridView.setLiveCells(liveList) }
     }
 }
--- app/src/main/java/samuel/exercise/homequiz/view/CellGridView.kt
+++ app/src/main/java/samuel/exercise/homequiz/view/CellGridView.kt
@@ -5,9 +5,13 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.util.Log
+import android.view.MotionEvent
+import android.view.MotionEvent.INVALID_POINTER_ID
 import android.view.View
 import androidx.core.content.ContextCompat
 import samuel.exercise.homequiz.R
+import kotlin.math.min
 
 class CellGridView : View {
 
@@ -15,14 +19,17 @@ class CellGridView : View {
     private val deadCellPaint: Paint
     private var dimenSizeX = 3
     private var dimenSizeY = 3
+    private var activePointerId = INVALID_POINTER_ID
+    private var lastTouchCellIdx = -1
+    private val liveCellSet = hashSetOf<Int>()
+
+    var cellTouchListener: CellTouchListener? = null
 
     constructor(context: Context) : this(context, null)
 
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
 
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
-        setBackgroundColor(Color.TRANSPARENT) // must set default background to enable onDraw callback
-
         liveCellPaint = Paint().apply { color = ContextCompat.getColor(context, R.color.colorAccent) }
         deadCellPaint = Paint().apply { color = Color.WHITE }
     }
@@ -30,6 +37,14 @@ class CellGridView : View {
     fun setDimenSize(sizeX: Int, sizeY: Int) {
         dimenSizeX = sizeX
         dimenSizeY = sizeY
+        invalidate()
+    }
+
+    fun setLiveCells(list: List<Int>) {
+        liveCellSet.clear()
+        for (cellIndex in list) {
+            liveCellSet.add(cellIndex)
+        }
         invalidate()
     }
 
@@ -42,7 +57,6 @@ class CellGridView : View {
 
         // Draw cell grid
         for (dy in 0 until dimenSizeY) {
-            var liveOrDead = dy % 2 == 0
             for (dx in 0 until dimenSizeX) {
                 val left = dx * cellWidth
                 val top = dy * cellHeight
@@ -51,13 +65,65 @@ class CellGridView : View {
                 var bottom = top + cellHeight
                 if (height - bottom < cellHeight) bottom = height
 
+                val liveOrDead = liveCellSet.contains(getCellIndex(dx, dy))
                 canvas.drawRect(
                     left, top, right, bottom,
                     if (liveOrDead) liveCellPaint else deadCellPaint
                 )
-                liveOrDead = !liveOrDead
             }
         }
     }
 
+    override fun onTouchEvent(event: MotionEvent): Boolean = when (event.actionMasked) {
+        MotionEvent.ACTION_DOWN -> {
+            event.actionIndex.also { pointerIndex ->
+                activePointerId = event.getPointerId(pointerIndex)
+                point2CellIndex(event.getX(pointerIndex), event.getY(pointerIndex)).let {
+                    lastTouchCellIdx = it
+                    onCellTouch(event, lastTouchCellIdx)
+                }
+            }
+            true
+        }
+        MotionEvent.ACTION_MOVE -> {
+            event.findPointerIndex(activePointerId).takeIf { it >= 0 }?.also { pointerIndex ->
+                point2CellIndex(event.getX(pointerIndex), event.getY(pointerIndex))
+                    .takeIf {
+                        it != lastTouchCellIdx
+                    }?.let {
+                        lastTouchCellIdx = it
+                        onCellTouch(event, lastTouchCellIdx)
+                    }
+            }
+            true
+        }
+        MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+            activePointerId = INVALID_POINTER_ID
+            true
+        }
+        else -> super.onTouchEvent(event)
+    }
+
+    private fun point2CellIndex(px: Float, py: Float): Int {
+        var dx = (px * dimenSizeX / measuredWidth).toInt()
+        var dy = (py * dimenSizeY / measuredHeight).toInt()
+        dx = min(dx, dimenSizeX - 1)
+        dy = min(dy, dimenSizeY - 1)
+        return getCellIndex(dx, dy)
+    }
+
+    private fun getCellIndex(dx: Int, dy: Int) = dy * dimenSizeY + dx
+
+    private fun onCellTouch(event: MotionEvent, cellIndex: Int) {
+        Log.d(TAG, "onCellTouch, ${event.actionMasked}, index=$cellIndex")
+        cellTouchListener?.onCellTouched(event, cellIndex)
+    }
+
+    interface CellTouchListener {
+        fun onCellTouched(event: MotionEvent, cellIndex: Int)
+    }
+
+    companion object {
+        const val TAG = "CellGridView"
+    }
 }
