82408d3c7eaea2c18f4ff06f5e700bc61ee184f7
Samuel <samuel@exercise.com>
2019-08-22 14:17:15 +0800

    Optimize algorithm of Containing7s

--- app/src/main/java/samuel/exercise/homequiz/q1/Containing7s.kt
+++ app/src/main/java/samuel/exercise/homequiz/q1/Containing7s.kt
@@ -21,4 +21,42 @@ object Containing7s {
         }
         return false
     }
+
+    fun gOptimize(n: Int): Int {
+        if (n < 10) {
+            return if (n < 7) 0 else 1
+        }
+
+        var quotient = n
+        var power = 10
+        var base7s = 1
+        var remainder = 0
+        var count = 0
+        while (quotient >= 10) {
+            // remainder to be count
+            remainder += (quotient % 10) * power / 10
+            quotient /= 10
+
+            if (quotient < 10) {
+                count = when {
+                    quotient < 7 -> quotient * base7s  // ex. 1xx
+                    quotient > 7 -> (quotient - 1) * base7s + power  // ex. 8xx
+                    else -> {
+                        // when quotient equal 7
+                        var subtotal = remainder + 1  // add all remainder, 7xx
+                        // calculate as 699
+                        subtotal += (quotient - 1) * base7s
+                        remainder = power - 1
+                        subtotal
+                    }
+                }
+            } else {
+                base7s = 9 * base7s + power
+                power *= 10
+            }
+        }
+        count += gOptimize(remainder)
+
+        return count
+    }
 }
--- app/src/test/java/samuel/exercise/homequiz/q1/Containing7sTest.kt
+++ app/src/test/java/samuel/exercise/homequiz/q1/Containing7sTest.kt
@@ -16,7 +16,13 @@ class Containing7sTest {
         assertEquals(19, Containing7s.g(100))
         assertEquals(38, Containing7s.g(200))
         assertEquals(55, Containing7s.g(280))
+        assertEquals(65, Containing7s.g(370))
+        assertEquals(71, Containing7s.g(376))
+        assertEquals(72, Containing7s.g(377))
+        assertEquals(73, Containing7s.g(378))
         assertEquals(271, Containing7s.g(1000))
+        assertEquals(476, Containing7s.g(1771))
+        assertEquals(496, Containing7s.g(1791))
     }
 
     @Test
@@ -25,5 +31,28 @@ class Containing7sTest {
         assertFalse(Containing7s.has7s(18))
         assertTrue(Containing7s.has7s(7))
         assertTrue(Containing7s.has7s(700))
+    }
+
+    @Test
+    fun gOptimize() {
+        assertEquals(0, Containing7s.gOptimize(-1))
+        assertEquals(0, Containing7s.gOptimize(0))
+        assertEquals(0, Containing7s.gOptimize(1))
+        assertEquals(1, Containing7s.gOptimize(7))
+        assertEquals(2, Containing7s.gOptimize(20))
+        assertEquals(8, Containing7s.gOptimize(70))
+        assertEquals(19, Containing7s.gOptimize(100))
+        assertEquals(38, Containing7s.gOptimize(200))
+        assertEquals(55, Containing7s.gOptimize(280))
+        assertEquals(19, Containing7s.gOptimize(100))
+        assertEquals(38, Containing7s.gOptimize(200))
+        assertEquals(55, Containing7s.gOptimize(280))
+        assertEquals(65, Containing7s.gOptimize(370))
+        assertEquals(71, Containing7s.gOptimize(376))
+        assertEquals(72, Containing7s.gOptimize(377))
+        assertEquals(73, Containing7s.gOptimize(378))
+        assertEquals(271, Containing7s.gOptimize(1000))
+        assertEquals(476, Containing7s.gOptimize(1771))
+        assertEquals(496, Containing7s.gOptimize(1791))
     }
 }
